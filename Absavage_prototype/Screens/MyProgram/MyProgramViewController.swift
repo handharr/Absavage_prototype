74f469909433ed8640c4afe9b4e5d0036ec7dcce
Puras Handharmahua <puras.handharmahua@mekari.com>
2022-09-08 20:57:26 +0800

    setup pods

--- Absavage_prototype/Screens/MyProgram/MyProgramViewController.swift
+++ Absavage_prototype/Screens/MyProgram/MyProgramViewController.swift
@@ -14,6 +14,9 @@ class MyProgramViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
         // - Initiate gradient layer
         let gradientLayer = CAGradientLayer()
         gradientLayer.frame = self.view.bounds
